b1d8da74fbe9b9c016cf49d642aac6618374a6e7
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-30 22:50:35 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild('myInput', { static: true }) myCustomInput;
+
+  ngOnInit() {
+    this.myCustomInput.nativeElement.focus();
+  }
+
   sendInput(input, channel) {
     // console.log(input);
     console.log(input.value);
